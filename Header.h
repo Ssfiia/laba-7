9de8c884e8a83551b220a332421a71bec46a929a
Сафия <124502236+Ssfiia@users.noreply.github.com>
2026-03-27 18:05:30 +0500

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+void Sort3(std::vector<float>& op, const std::string& filename);
+void ExamTaskC15(int k);
+struct C15 {
+    int prod;
+    int code;
+    int year;
+    int month;
+};
+void Five15(const std::string& inFile, const std::string& outFile);
+struct Student {
+    std::string surname;
+    int score;
+};
